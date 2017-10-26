e1d0292d4acc3ac0b66ccbf4e57a9d9bc4dec835
implicitdef <implicitdef@users.noreply.github.com>
2017-10-26 11:51:48 +0200

    drafting components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
+
+const PlayerScreen = ({number, word, isRevealed, onReveal, onHide, onForward, onBack}) => (
+  <div>
+    <button onClick={onBack}>
+      Joueur précédent
+    </button>
+    <h1>Joueur n°{number}</h1>
+    <button onClick={onForward}>
+      Joueur suivant
+    </button>
+    <div>
+      {isRevealed &&
+        <div onClick={onHide}>
+          {
+            word ? 
+              <pre>{word}</pre>
+              <p>Contre-espion</p>
+          }
+        </div>
+      }
+      {isRevealed || 
+        <button onClick={onReveal}>
+          Clique-ici pour révéler
+        </button>
+      }
+    </div>
+  </div>
+)
+
+
+class StatefulPlayerScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRevealed: false
+    };
+  }
+  
+  render() {
+    return <PlayerScreen
+      number={this.props.number}
+      word={this.props.word}
+      onBack={this.props.onBack}
+      onForward={this.props.onForward}
+      isRevealed={this.state.isRevealed}
+      onReveal={() => {this.setState({isRevealed: true});}}
+      onHide={() => {this.setState({isRevealed: false});}}
+    />
+  }
+}
+
+const PlayerScreens = ({words, currentPlayerNumber, onBack, onForward}) => (
+  <PlayerScreen
+    word={words[currentPlayerNumber]}
+    number={currentPlayerNumber + 1}
+    onBack={onBack}
+    onForward={onForward}
+  />
+);
+
+
+
 class App extends Component {
   render() {
     return (
